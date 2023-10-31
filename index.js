98545cd63b8a7a2adf848603d1fa607e6eace45b
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-10-31 18:36:37 +0800

    chore: added and test basic post gres installation

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-import  express from 'express';
-import getSightings from './utils.js'
-import cors from 'cors'
-import path from 'path'
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import DefaultRouter from './routers/sightings.js';
+import pg from 'pg';
+import dotenv from 'dotenv';
 
-import dotenv from 'dotenv'
 
-const PORT = process.env.PORT||3000;
+const {Client}= pg;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const envFilePath = '.env';
-dotenv.config({ path: path.normalize(envFilePath) });
-app.use(cors())
 
-app.get("/sightings", async (req, res) => {
-  const sightings = await getSightings();
-  res.json(sightings);
-});
+
+const pgConnectionConfigs = {
+  user:'Zach',
+  host:'localhost',
+  database:'Zach',
+  port:5432,
+}
+const client= new Client(pgConnectionConfigs);
+
+client.connect();
+
+const whenQueryDone= (error, result)=>{
+  if(error){
+    console.log('error:',error);
+  }
+
+  else{
+    console.log(result.rows);
+  }
+  client.end()
+}
+
+const sqlQuery= 'SELECT * FROM students';
+
+client.query(sqlQuery, whenQueryDone);
+
+
+
+dotenv.config({ path: path.normalize(envFilePath) });
+app.use(cors());
+
+app.use('/', DefaultRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Express app listening on port ${PORT}!`);
--- routers/sightings.js
+++ routers/sightings.js
@@ -21,7 +21,7 @@ DefaultRouter.get("/sightings/:REPORT_NUMBER", (req, res) => {
 
     if (record) {
       console.log('Found record:', record);
-      res.send({ success: true, data: record });
+      res.status(200).json({ success: true, data: record });
     } else {
       console.log('Record not found');
       res.send({ success: false, message: 'Record not found' });
